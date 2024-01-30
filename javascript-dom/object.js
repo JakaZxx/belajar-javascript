9ee01a64db3e021a13fa12dc804057eda652b9c4
JakaZxx <jaluanwar08@gmail.com>
2024-01-30 08:50:37 +0700

    Object JSON

--- javascript-dom/object.js
+++ javascript-dom/object.js
@@ -6,6 +6,17 @@ siswa.namaLengkap = function() {
     return this. namaDepan + " " +this. namaBelakang;
 };
 
+//object JSON
+var siswa2 = {
+    namaDepan : "yanto",
+    namaBelakang : "bambang",
+    alamat : "bandung",
+    namaLengkap : function(){
+        return this.namaDepan + " " + this.namaBelakang;
+    },
+};
+
 alert("Nama :" + siswa.namaLengkap());
+alert("Nama :" + siswa2.namaLengkap());
 
 // function namaLengkap(){}
